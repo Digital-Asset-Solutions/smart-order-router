8a373021d0cdeebc712e05d993d9324c0c2eb32b
Anthony <anthony.issa@hotmail.fr>
2025-10-15 11:44:44 +0200

    new subgrpah

--- src/providers/v3/subgraph-provider.ts
+++ src/providers/v3/subgraph-provider.ts
@@ -63,7 +63,7 @@ const SUBGRAPH_URL_BY_CHAIN: { [chainId in ChainId]?: string } = {
   [ChainId.EDEN_TESTNET]:
     'https://api.goldsky.com/api/public/project_cmfv8bj7kvt5b01xyc3ixbole/subgraphs/eden-testnet-v3-subgraph/1.0.0/gn',
   [ChainId.SOMNIA]:
-    'https://subgraph.somnia.network/dashboard/subgraph/somnia-v3/v0.0.1',
+    'https://api.subgraph.somnia.network/api/public/eba83aa2-ee9c-418a-96d8-838bcf96c8f0/subgraphs/somnia-v3/v0.0.1/gn',
 };
 
 /**
